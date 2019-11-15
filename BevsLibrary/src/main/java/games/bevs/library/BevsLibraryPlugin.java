1db44b58cf7499721fcb91a308afc125bf918052
Heath Logan Campbell <github@heathlogancampbell.com>
2019-11-15 17:00:53 +1300

    set up project

---
+++ BevsLibrary/src/main/java/games/bevs/library/BevsLibraryPlugin.java
@@ -0,0 +1,24 @@
+package games.bevs.library;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BevsLibraryPlugin extends JavaPlugin
+{
+    @Override
+    public void onLoad()
+    {
+
+    }
+
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+}
---
+++ BevsPermissions/src/main/java/games/bevs/permissions/BevsPermissionsPlugin.java
@@ -0,0 +1,8 @@
+package games.bevs.permissions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BevsPermissionsPlugin extends JavaPlugin
+{
+
+}
